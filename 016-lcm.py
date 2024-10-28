39fa7e2db0c91a11a05fe5508aa3e9672491ba11
rd_626 <rohandas.zero@gmail.com>
2024-10-29 00:49:45 +0530

    add more

---
+++ 016-lcm.py
@@ -0,0 +1,20 @@
+def hcf(a, b):
+    x = 1
+    highest_common_factor = 1
+
+    while x <= a and x <= b:
+        if (a % x == 0) and (b % x == 0):
+            highest_common_factor = x
+
+        x += 1
+
+
+    return highest_common_factor
+
+
+def lcm(num1,num2):
+    a = (num1*num2) / gcd(num1,num2)
+    print(f"The LCM of {num1}, {num2} is {a}")
+
+
+
