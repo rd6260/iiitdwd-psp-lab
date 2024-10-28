c0727c9e326caf62703a7c7a317905b919a139d1
rd_626 <rohandas.zero@gmail.com>
2024-10-28 23:32:53 +0530

    add more

--- 002-fibonacci-number.py
+++ 002-fibonacci-number.py
@@ -2,14 +2,21 @@
 
 
 def nth_fibo(n):
-    if n==1:
-        return 1
-    elif n==0:
+    if n <= 0:
+        print("Invalid input")
+    elif n == 1:
         return 0
-    else:
-        return nth_fibo(n-1)+nth_fibo(n-2)
+    elif n == 2:
+        return 1
 
-while True:
-    n=int(input("enter input\n"))
-    print("output is" ,nth_fibo(n))
+    a, b = 0, 1
+    for _ in range(2, n):
+        a, b = b, a + b
+
+    return b
+
+
+
+n=int(input("enter input\n"))
+print("output is" ,nth_fibo(n))
 
